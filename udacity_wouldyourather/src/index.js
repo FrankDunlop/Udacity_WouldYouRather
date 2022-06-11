b38cae9dd499a69512cad3a3951ce123984d5b44
Frank_Dunlop_init <fdunlop@initse.com>
2022-06-11 12:15:11 +0100

    create store and wrap provider components

--- udacity_wouldyourather/src/index.js
+++ udacity_wouldyourather/src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const store = createStore(reducer)
+
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
-);
-
+  </Provider>,
+  document.getElementById('root')
+)
