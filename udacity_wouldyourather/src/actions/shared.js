a1573d322f3df2f9933c98607ece96877a07368e
Frank_Dunlop_init <fdunlop@initse.com>
2022-06-11 11:55:37 +0100

    setup authed user

--- udacity_wouldyourather/src/actions/shared.js
+++ udacity_wouldyourather/src/actions/shared.js
@@ -1,6 +1,9 @@
 import { getInitialData } from "../utils/api"
 import { receiveQuestions } from "../actions/questions"
 import { receiveUsers } from "../actions/users"
+import { setAuthedUser } from "../actions/authedUser"
+
+const AUTHED_ID = 'fdunlop'
 
 //redux thunk pattern to make async request inside handleInitialData function
 //getInitialData returns promise that will pass an object with users and questions property
@@ -11,6 +14,7 @@ export function handleInitialData() {
         .then(({ users, questions }) => {
             dispatch(receiveUsers(users))
             dispatch(receiveQuestions(questions))
+            dispatch(setAuthedUser(AUTHED_ID))
     });
   };
 }
