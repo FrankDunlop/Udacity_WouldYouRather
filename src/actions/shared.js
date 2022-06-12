d5f8a00332d8765456db9a2d473253bd0c97c8cc
Frank_Dunlop_init <fdunlop@initse.com>
2022-06-12 10:50:33 +0100

    add loading bar

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,6 +2,7 @@ import { getInitialData } from "../utils/api"
 import { receiveQuestions } from "../actions/questions"
 import { receiveUsers } from "../actions/users"
 import { setAuthedUser } from "../actions/authedUser"
+import { showLoading, hideLoading } from "react-redux-loading"
 
 const AUTHED_ID = 'fdunlop'
 
@@ -10,11 +11,13 @@ const AUTHED_ID = 'fdunlop'
 //add users and questions to the redux store
 export function handleInitialData() {
   return (dispatch) => {
+    dispatch(showLoading())
     return getInitialData()
         .then(({ users, questions }) => {
             dispatch(receiveUsers(users))
             dispatch(receiveQuestions(questions))
             dispatch(setAuthedUser(AUTHED_ID))
+            dispatch(hideLoading())
     });
   };
 }
