1ce2dfd28d2c0b3d1f3ddb7150bbde5f3df70f4d
Frank_Dunlop_init <fdunlop@initse.com>
2022-06-12 13:28:43 +0100

    set login user, authedUser

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { setAuthedUser } from '../actions/authedUser';
 
 class Login extends Component {
+    handleUserLogin = id => {
+        this.props.dispatch(setAuthedUser(id))
+    }
 
   render() {
     const { userIds, users } = this.props;
-    console.log(this.props)
+
     return (
       <div>
             <h3> Login </h3>
             <ul>
                 {userIds.map((id) => (
-                    <li key={id}>
+                    <li key={id} onClick={() => this.handleUserLogin(id)}>
                         <div>
                             { users[id].name }
                         </div>
