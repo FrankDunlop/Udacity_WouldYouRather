149e9b2137e6ec8d758bb6393c71ad3a26bc2d54
Frank_Dunlop_init <fdunlop@initse.com>
2022-06-12 10:14:50 +0100

    add question component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Question from './Question'
 
 class Dashboard extends Component {
     render() {
-        //console.log(this.props)
+        console.log(this.props)
         return (
             <div>
                 <h3 className='center'> Questions</h3>
@@ -11,7 +12,7 @@ class Dashboard extends Component {
                     {this.props.questionIds.map((id) => (
                         <li key={id}>
                             <div>
-                                Question Id: {id}
+                                <Question Id={id} />
                             </div>
                         </li>
                     ))}
