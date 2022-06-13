0594b895f0184bf8b2ead42e29b8cc75bbb4b6f3
Frank_Dunlop_init <fdunlop@initse.com>
2022-06-13 10:04:48 +0100

    toggle answered questions display

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -2,49 +2,68 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class QuestionList extends Component {
-  render() {
-    const { questionIds, questions, answeredQuestions, unansweredQuestions } = this.props;
+    state = { 
+        showUnanswered: true,
+        toggleText: 'Show Answered Questions' 
+    }
 
-    console.log(this.props)
+    toggleQuestions = () => {
+        this.setState({ 
+            showUnanswered: !this.state.showUnanswered,
+            toggleText: this.state.showUnanswered ? 'Show Unanswered Questions' : 'Show Answered Questions'
+        })
+    }
+
+    render() {
+    const { questions, answeredQuestions, unansweredQuestions } = this.props;
+
     return (
         <div>
-            <div id='unanswered'>
-                {unansweredQuestions.map((id) => (
+            <input type="submit" value={this.state.toggleText} onClick={this.toggleQuestions} />
+            
+            {this.state.showUnanswered && (
+                <div id='unanswered'>
+                    {unansweredQuestions && unansweredQuestions.map((id) => (
+                        <li key={id}>
+                            {/* <div>Unanswered {questions[id].timestamp}</div> */}
+                            <div>{ questions[id].author } </div>
+                            <div>Would You Rather</div>
+                            <button>{ questions[id].optionOne.text } </button> or <button>{ questions[id].optionTwo.text }</button>
+                        </li>
+                    ))}
+                </div>
+            )}
+
+            {!this.state.showUnanswered && (
+                <div id='answered'>
+                {answeredQuestions && answeredQuestions.map((id) => (
                     <li key={id}>
-                        <div>Unanswered {questions[id].timestamp}</div>
+                        {/* <div>Answered {questions[id].timestamp}</div> */}
                         <div>{ questions[id].author } </div>
                         <div>Would You Rather</div>
                         <button>{ questions[id].optionOne.text } </button> or <button>{ questions[id].optionTwo.text }</button>
                     </li>
                 ))}
             </div>
-            <div id='answered'>
-                {answeredQuestions.map((id) => (
-                    <li key={id}>
-                        <div>Answered {questions[id].timestamp}</div>
-                        <div>{ questions[id].author } </div>
-                        <div>Would You Rather</div>
-                        <button>{ questions[id].optionOne.text } </button> or <button>{ questions[id].optionTwo.text }</button>
-                    </li>
-                ))}
-            </div>
+            )}
         </div>
     )}
 }
 
-function mapStateToProps({ authedUser, questions, users}, {id}) {
-    const question = questions[id]
+function mapStateToProps({ authedUser, questions, users}) {
+    //const question = questions[id]
     const user = users[authedUser]
     var answeredQuestions = []
     var unansweredQuestions = []
 
     if(user)
     {   
+        const userAnsweredQuestions = Object.keys(user.answers)
         const questionsId = Object.keys(questions)
             .sort((a, b) => questions[b].timestamp - questions[a].timestamp
         )
-        answeredQuestions = Object.keys(user.answers)
-        unansweredQuestions = questionsId.filter(id => !answeredQuestions.includes(id)).map(id => questions[id].id)
+        answeredQuestions = questionsId.filter(id => userAnsweredQuestions.includes(id)).map(id => questions[id].id)
+        unansweredQuestions = questionsId.filter(id => !userAnsweredQuestions.includes(id)).map(id => questions[id].id)
     }   
 
     return {
@@ -53,7 +72,7 @@ function mapStateToProps({ authedUser, questions, users}, {id}) {
         questions,
         answeredQuestions,
         unansweredQuestions,
-        question: question
+        //question: question
   }
 }
 
