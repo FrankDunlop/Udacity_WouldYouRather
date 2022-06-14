896a447b2a7812d82055c480608271738c8bf10e
Frank_Dunlop_init <fdunlop@initse.com>
2022-06-14 18:42:37 +0100

    fix typo in question id

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -10,7 +10,7 @@ export default function questions (state = {}, action) {
     case ADD_QUESTION :
       return {
          ...state,
-         [action.question.Id]: action.question
+         [action.question.id]: action.question
       }
       case ADD_QUESTION_VOTE :
         return {
