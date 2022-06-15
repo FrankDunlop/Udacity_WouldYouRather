a31362def1f99cccc917a842ab1b684b948b4534
Frank_Dunlop_init <fdunlop@initse.com>
2022-06-15 07:52:20 +0100

    add avatar to leaderboard

--- src/components/UserScores.js
+++ src/components/UserScores.js
@@ -6,6 +6,7 @@ class UserScores extends Component {
 
         return (
             <div >
+                <img  alt='pic' src={user.avatarURL}/>
                 <div>{user.name} scored {Object.keys(user.answers).length + Object.keys(user.questions).length}</div>
                 <div>Answered {Object.keys(user.answers).length} Questions</div>
                 <div>Asked {Object.keys(user.questions).length} Questions</div>
