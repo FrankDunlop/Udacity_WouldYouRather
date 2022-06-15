a2ed9f0c6e9014326e5d2dbb70dbfecb9f3ec0f2
Frank_Dunlop_init <fdunlop@initse.com>
2022-06-15 22:34:08 +0100

    change answered layout

--- src/components/Question.js
+++ src/components/Question.js
@@ -41,10 +41,11 @@ class Question extends Component {
 
                 { id && answered && (
                     <div>
-                        <img  alt='pic' src={'/images/' + users[questions[id].author].avatarURL}/>
+                        {/* <img  alt='pic' src={'/images/' + users[questions[id].author].avatarURL}/> */}
+                        <div>{users[questions[id].author].name} asked would you rather { questions[id].optionOne.text } or { questions[id].optionTwo.text }?</div>
                         <div>{user.name} would rather <span style={{ display: this.getAnsweredQuestion(1, user.answers[id]) }}>{ questions[id].optionOne.text }</span> <span style={{ display: this.getAnsweredQuestion(2, user.answers[id]) }}>{ questions[id].optionTwo.text }</span></div>
-                        <div>Option 1 has {questions[id].optionOne.votes.length} Votes, {Math.round(questions[id].optionOne.votes.length / (questions[id].optionOne.votes.length + questions[id].optionTwo.votes.length) * 100)}% of people voted for '{ questions[id].optionOne.text }'</div>
-                        <div>Option 2 has {questions[id].optionTwo.votes.length} Votes, {Math.round(questions[id].optionTwo.votes.length / (questions[id].optionOne.votes.length + questions[id].optionTwo.votes.length) * 100)}% of people voted for '{ questions[id].optionTwo.text }' </div>
+                        <div>{ questions[id].optionOne.text } has {questions[id].optionOne.votes.length} Votes, {Math.round(questions[id].optionOne.votes.length / (questions[id].optionOne.votes.length + questions[id].optionTwo.votes.length) * 100)}% of people voted for this option</div>
+                        <div>{ questions[id].optionTwo.text } has {questions[id].optionTwo.votes.length} Votes, {Math.round(questions[id].optionTwo.votes.length / (questions[id].optionOne.votes.length + questions[id].optionTwo.votes.length) * 100)}% of people voted for this option</div>
                     </div>
                 )}
           </div>
