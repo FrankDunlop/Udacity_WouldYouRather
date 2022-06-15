0e3c0dc0383f342ef92dc2ec832abc8aef53b62c
Frank_Dunlop_init <fdunlop@initse.com>
2022-06-15 09:32:24 +0100

    refactor login for dropdown list

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,40 +4,51 @@ import { Redirect } from 'react-router-dom'
 import { setAuthedUser } from '../actions/authedUser'
 
 class Login extends Component {
-    handleUserLogin = id => {
-        this.props.dispatch(setAuthedUser(id))
-    }
+  state = { 
+    userId: ''
+  }
+
+  LoginUser = () => {
+    this.props.dispatch(setAuthedUser(this.state.userId === '' ? this.props.userIds[0] : this.state.userId))
+  }
 
   render() {
-    const { authedUser, userIds, users } = this.props;
+    const { authedUser, userIds, users } = this.props
 
     if (authedUser) {
       return <Redirect to={'/questions'} />
+    }
+  
+    const handleChange = (e) => {
+        let userId = e.target.value
+        this.setState(() => ({
+          userId: userId
+        }))
     }
 
     return (
       <div>
             <h3> Login </h3>
-            <ul>
-                {userIds.map((id) => (
-                    <li key={id} onClick={() => this.handleUserLogin(id)}>
-                        <div>
-                            { users[id].name }
-                        </div>
-                    </li>
-                ))}
-            </ul>
+            <div>
+              <select onChange={handleChange}>
+                {userIds.map((id, i) => 
+                  <option key={i} value={id}>{users[id].name}</option>
+                )}
+              </select>
+            </div>
+            <button onClick={this.LoginUser}>Login</button>
       </div>
-    );
+    )
   }
 }
+
 
 function mapStateToProps({ authedUser, users }) {
   return {
     authedUser,
     userIds: Object.keys(users),
     users
-  };
+  }
 }
 
 export default connect(mapStateToProps)(Login);
