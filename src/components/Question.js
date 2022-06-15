159f7ddfa67c4476034d9245ad03bbb54077a42c
Frank_Dunlop_init <fdunlop@initse.com>
2022-06-15 22:21:54 +0100

    bugfix for displaying choice

--- src/components/Question.js
+++ src/components/Question.js
@@ -9,10 +9,10 @@ class Question extends Component {
 
     getAnsweredQuestion = (option, userSelection) => {
         if(option === 1){
-            return userSelection === 'optionOne' ? 'none' : 'line-through' 
+            return userSelection === 'optionOne' ? '' : 'none' 
         }
         if(option === 2){
-            return userSelection === 'optionTwo' ? 'none' : 'line-through' 
+            return userSelection === 'optionTwo' ? '' : 'none' 
         }
     }
 
@@ -42,7 +42,7 @@ class Question extends Component {
                 { id && answered && (
                     <div>
                         <img  alt='pic' src={'/images/' + users[questions[id].author].avatarURL}/>
-                        <div>{user.name} would rather <span style={{ textDecorationLine: this.getAnsweredQuestion(1, user.answers[id]) }}>{ questions[id].optionOne.text }</span> <span style={{ textDecorationLine: this.getAnsweredQuestion(2, user.answers[id]) }}>{ questions[id].optionTwo.text }</span></div>
+                        <div>{user.name} would rather <span style={{ display: this.getAnsweredQuestion(1, user.answers[id]) }}>{ questions[id].optionOne.text }</span> <span style={{ display: this.getAnsweredQuestion(2, user.answers[id]) }}>{ questions[id].optionTwo.text }</span></div>
                         <div>Option 1 has {questions[id].optionOne.votes.length} Votes, {Math.round(questions[id].optionOne.votes.length / (questions[id].optionOne.votes.length + questions[id].optionTwo.votes.length) * 100)}% of people voted for '{ questions[id].optionOne.text }'</div>
                         <div>Option 2 has {questions[id].optionTwo.votes.length} Votes, {Math.round(questions[id].optionTwo.votes.length / (questions[id].optionOne.votes.length + questions[id].optionTwo.votes.length) * 100)}% of people voted for '{ questions[id].optionTwo.text }' </div>
                     </div>
